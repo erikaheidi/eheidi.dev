8b19894e7673b4c878f57a4468ba2906cb630799
Your Name <erika@erikaheidi.com>
2023-11-22 08:25:36 +0100

    Adding first version of br site

---
+++ br/config.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'site_name' => 'EHeidi.dev',
+    'site_description' => 'Eu escrevo sobre as coisas que gosto de fazer em meu tempo livre. Código, Linux, Design e Impressão 3D, e mais umas coisas ai...',
+    'data_path' => __DIR__ . '/content',
+    'output_path' => __DIR__ . '/public',
+    'cache_path' => __DIR__ . '/var/cache',
+    // Set site_index if you want a custom index page
+    'site_index' => 'about/erika',
+    'site_index_tpl' => 'content/index.html.twig',
+];
